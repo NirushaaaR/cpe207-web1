13d8f07183635c85202d7e16c963f2c931cb416d
NirushaaaR <nirush_srijaimoon@cmu.ac.th>
2019-03-11 16:12:07 +0700

    first commit!!

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("hello world");
+
+var str = "";
+
+for(var i=0; i<10 ; i++){
+    console.log(i+1);
+    str += (i+1) + "<br>";
+}
+
+document.querySelector("#output").innerHTML = str;
